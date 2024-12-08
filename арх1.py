7ba9e6764d3d6d6934203894fcb0c7396e37189b
angeshass <angelinanik155@mail.ru>
2024-12-08 03:38:28 +0300

    v2 добавила клиент, но не сервер

--- арх1.py
+++ арх1.py
@@ -2,22 +2,31 @@ import socket
 import time
 import os
 import fcntl
+import sys, errno
+
+'''тесты:
+просто клиент (запустили без сервера) - обработано в ошибке мейна, сразу умирает
+клиент, но не сервер (убили сервер) - обработано в ошибке отправления пинга, клиент пытается отправить и получить, но не может
+1+ клиент - обработано в открытии файла, нельзя
+1+ сервер - обработано в мейне, нельзя
+убиваем клиента - сервер тоже умирает (плохо, исправить бы?)
+'''
 
 def start_client(host='127.0.0.1', port=65432):
     lock_file = "client_lock.txt"
 
     try:
         fp = open(lock_file, 'w')
-        fcntl.flock(fp, fcntl.LOCK_EX | fcntl.LOCK_NB)  #
+        fcntl.flock(fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
     except IOError:
         print("Процесс одного клиента уже запущен. Завершение работы.")
         return
 
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((host, port)) #
+            s.connect((host, port))
 
-            client_address = s.getsockname() #
+            client_address = s.getsockname()
             client_code = f"{client_address[0]}:{client_address[1]}"
 
             with open('client_code.txt', 'w') as f:
@@ -28,7 +37,7 @@ def start_client(host='127.0.0.1', port=65432):
             while True:
                 try:
                     request = "ping"
-                    print(f"НЕЕЕТ: {request}")
+                    print(f"Отправка сообщения: {request}")
                     s.sendall(request.encode())
 
                     data = s.recv(1024)
@@ -41,6 +50,10 @@ def start_client(host='127.0.0.1', port=65432):
                         print("Ошибка: непредвиденный ответ")
 
                     time.sleep(1)
+                except IOError as e:
+                    if e.errno == errno.EPIPE:
+                        print("На другом конце конвейера нет считывания процесса")
+                        break
 
                 except Exception as e:
                     print(f"Произошла ошибка: {e}")
@@ -53,4 +66,10 @@ def start_client(host='127.0.0.1', port=65432):
 
 
 if __name__ == "__main__":
-    start_client()
+    try:
+        start_client()
+
+    except ConnectionRefusedError:
+        print("Невозможно подключиться к серверу")
+
+
